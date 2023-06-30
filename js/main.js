424f31c4f37b40e62a50509487ef15c0a1f1a51a
agathasoaresx <agatacsoares@gmail.com>
2023-06-29 21:58:10 -0300

    adicionando filtro

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const characterId = document.getElementById('characterId');
 const btnGo = document.getElementById('btn-go');
 const btnEdit = document.getElementById('btn-edit');
+const btnFilter = document.getElementById('btn-filter');
 const checkStatus = document.getElementById('status');
 const checkSpecies = document.getElementById('species');
 const checkGender = document.getElementById('gender');
@@ -27,25 +28,19 @@ const handleModalClose = (event) => {
 
 // FETCH API (pegando os dados)
 
-const fetchApi = (value) => {
-    const result = fetch(`https://rickandmortyapi.com/api/character/${value}`)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      return data;
-    });
+const fetchApi = async (value) => {
+    const response = await fetch(`https://rickandmortyapi.com/api/character/${value}`)
+    const data = await response.json()
+    return data;
+}
+
+
+// PESQUISANDO ID DO PERSONAGEM 
+
+  btnGo.addEventListener('click', async (event) => {
+  event.preventDefault();
   
-    return result;
-  }
-
- 
-
-  // PESQUISANDO ID DO PERSONAGEM 
-
-   btnGo.addEventListener('click', async (event) => {
-    event.preventDefault();
-  
-  // verificando se o id existe
+// verificando se o id existe
 
     if(characterId.value === ''){
       alert("Digite o número de um personagem!");
@@ -80,74 +75,89 @@ const fetchApi = (value) => {
 
 // Eventos de mouse leave e mouse enter
 
-const handleTickerMouseEnter = (event) => {
+const handleCardMouseEnter = (event) => {
     const card = event.target
     const btnClose = card.querySelector(".btn-close")
     const btnEdit = card.querySelector(".btn-edit")
     btnClose.style.display = "block"
     btnEdit.style.display = "block"
+    card.style.backgroundColor = "#678C4Da2"
+    card.style.transform = "scale(1.05, 1.05)"
+
 }
 
-const handleTickerMouseLeave = (event) => {
+const handleCardMouseLeave = (event) => {
   const card = event.target
   const btnClose = card.querySelector(".btn-close")
   const btnEdit = card.querySelector(".btn-edit")
   btnClose.style.display = "none"
   btnEdit.style.display = "none"
+  card.style.backgroundColor = "#282A36a2"
+  card.style.transform = "none"
+  
 }
+
 
 const addCardsEvents = () => {
     const cards = document.querySelectorAll(".card")
     cards.forEach((card) => {
-        card.addEventListener("mouseenter", handleTickerMouseEnter)
-        card.addEventListener("mouseleave", handleTickerMouseLeave)
+        card.addEventListener("mouseenter", handleCardMouseEnter)
+        card.addEventListener("mouseleave", handleCardMouseLeave)
     })
 }
 
+// função filtro 
 
+const filtrarDados = async (event) => {
+  //peg o cards-list e limpa 
+  event.preventDefault()
+  const cardsList = document.querySelector('#cards-list')
+  cardsList.innerHTML = ''
 
-const renderCards = () => {
-  const divCardsList = document.querySelector("#cards-list")
-  divCardsList.innerHTML = ''
+  check = document.querySelector('input[name="gender"]:checked').value
+  //console.log(check)
 
-    if(characterId.value === ''){
-      alert("Digite o número de um personagem!");
-    } else {
+  try {
+    const response = await fetch(`https://rickandmortyapi.com/api/character/?gender=${check}`)
+    const data = await response.json()
+    //console.log(data)
 
-    const newCard = 
-            `<div class="card" id="character">
-            <button class="btn-close" onclick="removeCard(event)">✖</button>
-            <button class="btn-edit" onclick="editCard(event)">🖉</button>
-            <div class="card-header"> 
-              <p class="card-title">${result.name}</p>
-            </div>
-            <div class="card-img">
-              <img src="${result.image}" alt="${result.name}"/>
-            </div>
-            <div class="card-body">
-              <p><b><span>Gender:<span></b> ${result.gender}</p>
-              <p><b><span>Species:<span></b> ${result.species}</p>
-              <p><b><span>Status:<span></b> ${result.status}</p>
-              <p><b><span>Origin:<span></b> ${result.origin.name}</p>
-             
-            </div>
-          </div>
-             `
-            const cardsList = document.querySelector("#cards-list")
-            cardsList.innerHTML = newCard + cardsList.innerHTML
-            addCardsEvents()
-            closeModal('#add-character')
+    for (let i = 0; i < data.results.length; i++) {
+
+      const nomePersonagem = data["results"][i]["name"]
+      console.log(data["results"][i]["name"])
+      const especiePersonagem = data["results"][i]["species"]
+      const statusPersonagem = data["results"][i]["status"]
+      const imgPersonagem = data["results"][i]["image"]
+
+      const newCard = 
+      `<div class="card">
+      <button class="btn-close" onclick="removeCard(event)">✖</button>
+      <button class="btn-edit" onclick="openModal('#edit-card')">🖉</button>
+      <div class="card-header"> 
+        <p class="card-title">${nomePersonagem}</p>
+      </div>
+      <div class="card-img">
+        <img src="${imgPersonagem}" alt="${nomePersonagem}"/>
+      </div>
+      <div class="card-body">
+        <p id="p-species"><b><span>Species:<span></b> ${especiePersonagem}</p>
+        <p id="p-status"><b><span>Status:<span></b> ${statusPersonagem}</p>     
+      </div>
+    </div>
+       `
+      const cardsList = document.querySelector("#cards-list")
+      cardsList.innerHTML = newCard + cardsList.innerHTML
+      addCardsEvents()
+      closeModal('#add-character')
     }
-}
+    
+  }catch(error) {
+    alert(error)
+  }
 
-const infos = ['name', 'status', 'species', 'gender', 'origin', 'episode'];
- const newInfos = {
-   name: 'Nome',
-   status: 'Status',
-   species: 'Espécie',
-   gender: 'Gênero',
-   origin: 'Origem'
- }
+  closeModal('#filter')
+}
 
 // removendo e editando o card
 
@@ -163,6 +173,7 @@ const editCard = (event) => {
 
   if(checkStatus.checked){
     const btnEdit = event.target
+
     const pStatus = document.getElementById('p-status')
     pStatus.remove()
     closeModal('#edit-card')
@@ -197,5 +208,5 @@ modal.addEventListener("click", handleModalClose)
 
 
 
-//addCardsEvents()
+addCardsEvents()
 
